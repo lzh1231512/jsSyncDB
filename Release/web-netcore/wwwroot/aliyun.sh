ae2a5eb77ab74386dbaa5f1fdd63139ac6742536
lzh <lzhenhi@sina.com>
2019-08-11 23:47:25 +0800

    1

--- Release/web-netcore/wwwroot/aliyun.sh
+++ Release/web-netcore/wwwroot/aliyun.sh
@@ -4,6 +4,7 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum makecache fast
 yum -y install docker-ce
+mkdir /etc/docker
 echo -e "{ \n\
     \"registry-mirrors\": [\"https://registry.docker-cn.com\",\"http://hub-mirror.c.163.com\"] \n\
 }" > /etc/docker/daemon.json
